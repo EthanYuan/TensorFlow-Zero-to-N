32c8e31e3754b17f24d51338a9b4fe8e3b762600
Ethan <yuanchengxing@hotmail.com>
2017-06-08 17:07:43 +0800

    update 2-manual.py.

--- 2-manual.py
+++ 2-manual.py
@@ -1,25 +1,19 @@
 import tensorflow as tf
 
 node1 = tf.constant(3.0, tf.float32)
-node2 = tf.constant(4.0) # also tf.float32 implicitly
-print(node1, node2)
+node2 = tf.constant(4.0)  # also tf.float32 implicitly
+node3 = tf.add(node1, node2)
 
 sess = tf.Session()
-print(sess.run([node1, node2]))
-
-node3 = tf.add(node1, node2)
 print("node3: ", node3)
-print("sess.run(node3): ",sess.run(node3))
+print("sess.run(node3): ", sess.run(node3))
 
 a = tf.placeholder(tf.float32)
 b = tf.placeholder(tf.float32)
 adder_node = a + b  # + provides a shortcut for tf.add(a, b)
 
-print(sess.run(adder_node, {a: 3, b:4.5}))
-print(sess.run(adder_node, {a: [1,3], b: [2, 4]}))
-
-add_and_triple = adder_node * 3.
-print(sess.run(add_and_triple, {a: 3, b:4.5}))
+print(sess.run(adder_node, {a: 3, b: 4.5}))
+print(sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
 
 W = tf.Variable([.3], tf.float32)
 b = tf.Variable([-.3], tf.float32)
@@ -29,14 +23,9 @@ linear_model = W * x + b
 init = tf.global_variables_initializer()
 sess.run(init)
 
-print(sess.run(linear_model, {x:[1,2,3,4]}))
-
-y = tf.placeholder(tf.float32)
-squared_deltas = tf.square(linear_model - y)
-loss = tf.reduce_sum(squared_deltas)
-print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
+print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
 
 fixW = tf.assign(W, [-1.])
 fixb = tf.assign(b, [1.])
 sess.run([fixW, fixb])
-print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
+print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
