942305f4e4111d3dc14bcad8acf7348e5ade5ec0
Ethan <yuanchengxing@hotmail.com>
2017-08-14 18:36:01 +0800

    update test of trained model.

--- tf_12_mnist_softmax.py
+++ tf_12_mnist_softmax.py
@@ -65,7 +65,7 @@ def main(_):
         sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
     # Test trained model
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    correct_prediction = tf.equal(tf.argmax(tf.nn.softmax(y), 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     print(sess.run(accuracy, feed_dict={x: mnist.test.images,
                                         y_: mnist.test.labels}))
