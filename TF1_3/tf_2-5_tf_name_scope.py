d7fc5f8785a39f68327d0c73a7839b04e7825f63
Ethan <yuanchengxing@hotmail.com>
2017-10-29 16:08:29 +0800

    add example for tf.name_scope and tf.variable_scope.

--- TF1_3/tf_2-5_tf_name_scope.py
+++ TF1_3/tf_2-5_tf_name_scope.py
@@ -1,34 +1,25 @@
 import tensorflow as tf
 
 with tf.name_scope('V1'):
-    a1 = tf.get_variable(name='a1', shape=[1],
-                         initializer=tf.constant_initializer(1))
-
-    a2 = tf.Variable(tf.random_normal(
+    a1 = tf.Variable(tf.random_normal(
         shape=[2, 3], mean=0, stddev=1))
-    a3 = tf.Variable([100])
-
-with tf.variable_scope("V2"):
-    b1 = tf.get_variable(
-        name='a1', shape=[1], initializer=tf.constant_initializer(1))
-    b2 = tf.Variable(tf.random_normal(
-        shape=[2, 3], mean=0, stddev=1), name='a2')
-
-with tf.variable_scope("V2", reuse=True):
-    c3 = tf.get_variable('a1')
+    a2 = tf.Variable([(100, 100, 100), (50, 50, 50.0)])
+    a3 = tf.add(a1, a2)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    assert a1.name == 'a1:0'
-    assert sess.run(a1) == [1.]
-    assert a2.name == 'V1/Variable:0'
-    assert a3.name == 'V1/Variable_1:0'
+    assert a1.name == 'V1/Variable:0'
+    assert a2.name == 'V1/Variable_1:0'
+    assert a3.name == 'V1/Add:0'
 
-    assert b1.name == 'V2/a1:0'
-    assert b2.name == 'V2/a2:0'
+with tf.name_scope("V2"):
+    a1 = tf.Variable([1], name='a1')
+    a2 = tf.Variable(tf.random_normal(
+        shape=[2, 3], mean=0, stddev=1), name='a2')
 
-    assert c3.name == 'V2/a1:0'
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
 
-assert a1.name == 'a1:0'
-assert a2.name == 'V1/Variable:0'
+    assert a1.name == 'V2/a1:0'
+    assert a2.name == 'V2/a2:0'
---
+++ TF1_3/tf_2-5_tf_variable_scope.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+
+with tf.name_scope('V1'):
+    a1 = tf.get_variable(name='a1', shape=[1],
+                         initializer=tf.constant_initializer(1))
+
+    a2 = tf.Variable(tf.random_normal(
+        shape=[2, 3], mean=0, stddev=1))
+    a3 = tf.Variable([100])
+    a4 = tf.add(a1, a2)
+
+with tf.variable_scope("V2"):
+    b1 = tf.get_variable(
+        name='a1', shape=[1], initializer=tf.constant_initializer(1))
+    b2 = tf.Variable(tf.random_normal(
+        shape=[2, 3], mean=0, stddev=1), name='a2')
+
+with tf.variable_scope("V2", reuse=True):
+    c3 = tf.get_variable('a1')
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    assert a1.name == 'a1:0'
+    assert sess.run(a1) == [1.]
+    assert a2.name == 'V1/Variable:0'
+    assert a3.name == 'V1/Variable_1:0'
+    assert a4.name == 'V1/Add:0'
+
+    assert b1.name == 'V2/a1:0'
+    assert b2.name == 'V2/a2:0'
+
+    assert c3.name == 'V2/a1:0'
+
+assert a1.name == 'a1:0'
+assert a2.name == 'V1/Variable:0'
