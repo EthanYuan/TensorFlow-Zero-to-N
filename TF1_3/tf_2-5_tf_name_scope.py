403f54587ad718b8bd3c907e85b9e3ab251c69b4
Ethan <yuanchengxing@hotmail.com>
2017-10-30 19:12:48 +0800

    update code.

--- TF1_3/tf_2-5_tf_name_scope.py
+++ TF1_3/tf_2-5_tf_name_scope.py
@@ -1,25 +1,24 @@
 import tensorflow as tf
 
 with tf.name_scope('V1'):
-    a1 = tf.Variable(tf.random_normal(
-        shape=[2, 3], mean=0, stddev=1))
-    a2 = tf.Variable([(100, 100, 100), (50, 50, 50.0)])
-    a3 = tf.add(a1, a2)
+    a1 = tf.Variable([50])
+    a2 = tf.Variable([100], name='a1')
+    d5 = a2
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-
-    assert a1.name == 'V1/Variable:0'
-    assert a2.name == 'V1/Variable_1:0'
-    assert a3.name == 'V1/Add:0'
+assert a1.name == 'V1/Variable:0'
+assert a2.name == 'V1/a1:0'
 
 with tf.name_scope("V2"):
-    a1 = tf.Variable([1], name='a1')
-    a2 = tf.Variable(tf.random_normal(
-        shape=[2, 3], mean=0, stddev=1), name='a2')
+    a1 = tf.add(a1, a2, name="Add_Variable_a1")
+    a2 = tf.multiply(a1, a2, name="Add_Variable_a1")
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    assert a1.name == 'V2/a1:0'
-    assert a2.name == 'V2/a2:0'
+    assert a1.name == 'V2/Add_Variable_a1:0'
+    assert sess.run(a1) == 150
+    assert a2.name == 'V2/Add_Variable_a1_1:0'
+    assert sess.run(a2) == 15000
+
+a2 = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='V1/a1:0')[0]
+assert a2.name == 'V1/a1:0'
